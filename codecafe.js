5b7f60b0602207e0d9645e1119575d8de5b9007d
sarahmuir88 <sarahmuir88@gmail.com>
2016-12-05 19:48:53 +0000

    Final

---
+++ codecafe.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+	$("li #about-us").hover(function(){
+			$(this).css("background-color", "black");
+			$(this).css("color", "white");
+		}, function(){
+			$(this).css("background-color", "rgba(0, 0, 0, 0.0");
+			$(this).css("color", "white");
+		});
+	$("li #sessions").hover(function(){
+			$(this).css("background-color", "black");
+			$(this).css("color", "white");
+		}, function(){
+			$(this).css("background-color", "rgba(0, 0, 0, 0.0");
+			$(this).css("color", "white");
+		});
+	$("li #contact-us").hover(function(){
+			$(this).css("background-color", "black");
+			$(this).css("color", "white");
+		}, function(){
+			$(this).css("background-color", "rgba(0, 0, 0, 0.0");
+			$(this).css("color", "white");
+		});
+	$("i .fa fa-twitter").hover(function(){
+			$(this).css("background-color", "white");
+			$(this).css("color", "#00aced");
+		}, function(){
+			$(this).css("background-color", "rgba(0, 0, 0, 0.0");
+			$(this).css("color", "white");
+		});
+	$("i .fa fa-facebook").hover(function(){
+			$(this).css("background-color", "#3b5998");
+		}, function(){
+			$(this).css("background-color", "rgba(0, 0, 0, 0.0");
+		});
+	$("i .fa fa-linkedin").hover(function(){
+			$(this).css("background-color", "#007bb5");
+		}, function(){
+			$(this).css("background-color", "rgba(0, 0, 0, 0.0");
+		});
+});
+
+			
+
